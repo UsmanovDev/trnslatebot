2321d03b8a7f4b73345792497486a7a59651900f
muhammadkhanusmanov <uqaxromon@gmail.com>
2023-11-21 22:27:05 +0500

    init

--- db.py
+++ db.py
@@ -1,4 +1,5 @@
-import json 
+import json
+import json
 from datetime import datetime, timedelta
 import pytz
 class DB:
@@ -16,41 +17,41 @@ class DB:
     def save(self):
         with open('db.json', 'w') as f:
             json.dump(self.db, f, indent=4)
-    
+
     def starting(self,chat_id):
         self.db["users"][str(chat_id)] = 'en-uz'
         return None
-    
+
     def change(self,chat_id,til):
         self.db["users"][str(chat_id)] = til
         return None
-    
+
     def get_lang(self,chat_id):
         return self.db["users"][str(chat_id)]
-    
+
     def check_admins(self,chat_id):
         if str(chat_id) in self.db["admins"].keys():
             return True
         return False
-    
+
     def allusers(self):
         return self.db["users"].keys()
-    
+
     def ruxsatlar(self,chat_id):
         return self.db['admins'][str(chat_id)]
-    
+
     def rfwd(self,chat_id, cmnd=False):
         self.db['admins'][str(chat_id)]['fwdmsg'] = cmnd
         return None
-    
+
     def rmsg(self,chat_id, cmnd=False):
         self.db['admins'][str(chat_id)]['msg'] = cmnd
         return None
-    
+
     def changer(self,chat_id,i,cmnd=False):
         self.db['admins'][str(chat_id)][i]=cmnd
         return None
-    
+
     def add(self,chat_id,i,adding):
         if i=='admin':
             self.db['admins'][str(chat_id)]={
@@ -64,11 +65,11 @@ class DB:
         else:
             self.db[i].append(adding)
         return None
-    
+
     def delete(self, chat_id):
         self.db['admins'].pop(str(chat_id))
         return None
-    
+
     def channel(self,username,cmd):
         if cmd=='delete':
             try:
@@ -84,26 +85,28 @@ class DB:
                 return False
     def channels(self):
         return self.db['channels']
-    
+
     def alladmins(self):
         return self.db['admins'].keys()
-    
+
     def groups(self):
         return self.db['groups']
-    
+
     def addgroup(self,id):
         self.db['groups'].append(str(id))
         return None
-    
-    def kunlikobuna(self,b=True):
+
+    def kunlikobuna(self,chat_id,b=True):
         tashkent_timezone = pytz.timezone('Asia/Tashkent')
         current_time = datetime.now()
         tashkent_time = str(current_time.astimezone(tashkent_timezone).day)
-        if b:
-            if tashkent_time==self.db['obunachi']:
-                self.db['obunachi'][1] += 1
+        a=list(self.db["users"].keys())
+        if b and not (str(chat_id) in a):
+            if tashkent_time==self.db['obunachi'][0]:
+                self.db['obunachi'][1] = self.db['obunachi'][1]+1
                 return None
             else:
+                self.db['obunachi'][0] = tashkent_time
                 self.db['obunachi'][1] = 1
                 return None
         else:
